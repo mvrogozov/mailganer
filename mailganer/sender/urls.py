3c3dff06a55156770b61852355de4215c278b205
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-11-16 19:59:14 +0300

    make api

--- mailganer/sender/urls.py
+++ mailganer/sender/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from .views import SenderView, PixelView
+from .views import PixelView, SenderApiView
 
 app_name = 'sender'
 
 urlpatterns = [
-    url(r'^index/', SenderView.as_view(), name='sender_index'),
+    url(r'^send/', SenderApiView.as_view(), name='sender_api'),
     url(r'^open-tracking/(?P<user>[0-9]+)/$', PixelView.as_view(), name='pixel_view')
 ]
--- mailganer/sender/views.py
+++ mailganer/sender/views.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
 import logging
-from django.views.generic import TemplateView, View
 from django.conf import settings 
-from django.http import HttpResponse, request
 from .utils import send_email
 from .models import Buyers
 from logging.handlers import RotatingFileHandler
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 LOG_FILE_NAME = 'email_sender.log'
+SUBJECT = 'Тема письма'
+EMAIL_TEMPLATE = 'email_template_2.html'
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = RotatingFileHandler(
@@ -24,43 +27,46 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-class SenderView(View):
-    template_name = 'sender/index.html'
-    data = {'name': 'Petya', 'age': 37}
-    def get(self, request, *args, **kwargs):
-        buyers = Buyers.objects.all()
-        print(Buyers.objects.count())
 
+class SenderApiView(APIView):
+    """
+    Send emails.
+    """
+    def get(self, request):
+        buyers = Buyers.objects.all()
+        timeout = int(request.data.get('timeout', 0))
         for buyer in buyers:
             data={
-                'name': u'%s' % buyer.name,
+                'name': buyer.name,
                 'surname': buyer.surname,
                 'birthday': buyer.birth_date,
                 'domain': settings.PROJECT_DOMAIN,
                 'email': buyer.email,
                 'id': buyer.id
             }
-            print('data= ', data)
-            print(buyer.email)
-            print(settings.PROJECT_DOMAIN)
-            send_email.delay(
-                subject='Тема письма',
-                template='email_template_2.html',
-                data=data,
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[buyer.email]
+            send_email.apply_async(
+                kwargs={
+                    'subject': SUBJECT,
+                    'template': EMAIL_TEMPLATE,
+                    'data': data,
+                    'from_email': settings.EMAIL_HOST_USER,
+                    'recipient_list': [buyer.email]
+                },
+                countdown=timeout
             )
-        return HttpResponse('done')
-        
+        return Response("OK", status=status.HTTP_200_OK)
 
-class PixelView(View):
-    template_name = 'sender/index.html'
+
+class PixelView(APIView):
+    """
+    Email reading check.
+    """
     def get(self, request, *args, **kwargs):
         try:
             image_data = open(os.path.join(settings.MEDIA_ROOT, 'pixel.png'), 'rb').read()
         except Exception:
-            print('Error opening pixel file')
-            return None
+            logger.error('Pixel picture error')
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         user_id = kwargs.get('user')
         logger.info('User '+user_id+' opened email')
-        return HttpResponse(image_data, content_type="image/png")
+        return Response(image_data, content_type='image/png')
